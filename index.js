fe2a78224c1ed734ee93f639ac101a0574625988
La-Keisha Towner <ltowner622@gmail.com>
2017-10-29 18:26:00 -0400

    add router for app

--- index.js
+++ index.js
@@ -1,4 +1,33 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
+import React, { Component } from 'react';
 
-AppRegistry.registerComponent('gitHubUserFinder', () => App);
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  NavigatorIOS
+} from 'react-native';
+
+import Main from './App/Component/Main';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#111111"
+  },
+});
+
+export default class gitHubUserFinder extends Component {
+  render() {
+    return(
+      <NavigatorIOS 
+      initialRoute={{ 
+        component: Main,
+        title: 'Find a Github User'
+      }}
+      style={styles.container}
+      />
+    );
+  }
+}
+AppRegistry.registerComponent("gitHubUserFinder", () => gitHubUserFinder);
