64d003cd969c0b496207dd9fc3e5d1f6536f658f
La-Keisha Towner <ltowner622@gmail.com>
2017-10-29 22:47:15 -0400

    Dashboard

--- App/Component/Dashboard.js
+++ App/Component/Dashboard.js
@@ -1,5 +1,15 @@
 import React, { Component } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import Badge from "./Badge";
+import Profile from "./Profile";
+import Repositories from "./Repositories";
+import { API } from "../Utils/GithubApi";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  TouchableHighlight
+} from "react-native";
 
 var styles = StyleSheet.create({
   container: {
@@ -13,15 +23,71 @@ var styles = StyleSheet.create({
     fontSize: 24,
     color: "white",
     alignSelf: "center"
+  },
+  bioText: {
+    fontSize: 15,
+    color: "#203525",
+    padding: 20,
+    alignSelf: "center"
   }
 });
 
 export default class Dashboard extends Component {
+  makeBackground(btn) {
+    var obj = {
+      flexDirection: "row",
+      alignSelf: "stretch",
+      justifyContent: "center",
+      flex: 1
+    };
+
+    if (btn === 0) {
+      obj.backgroundColor = "#5b9693";
+    } else if (btn === 1) {
+      obj.backgroundColor = "#a6bcbb";
+    }
+    return obj;
+  }
+  goToProfile() {
+    this.props.navigator.push({
+      name: "Profile",
+      component: Profile,
+      passProps: { userInfo: this.props.userInfo }
+    });
+  }
+  goToRepos() {
+    API.getRepos(this.props.userInfo.login).then(res => {
+      console.log(res);
+      this.props.navigator.push({
+        title: "Repositories Page",
+        component: Repositories,
+        passProps: {
+          userInfo: this.props.userInfo,
+          repos: res
+        }
+      });
+    });
+  }
   render() {
     return (
       <View style={styles.container}>
-        <Text> Dashboard. </Text>
-        <Text> {JSON.stringify(this.props.userInfo)} </Text>
+        <Image
+          source={{ uri: this.props.userInfo.avatar_url }}
+          style={styles.image}
+        />
+        <Text style={styles.bioText}>{this.props.userInfo.bio}</Text>
+        <TouchableHighlight
+          style={this.makeBackground(0)}
+          onPress={this.goToProfile.bind(this)}
+        >
+          <Text style={styles.buttonText}>View Profile</Text>
+        </TouchableHighlight>
+        <TouchableHighlight
+          style={this.makeBackground(1)}
+          onPress={this.goToRepos.bind(this)}
+        >
+          <Text style={styles.buttonText}>View Repos</Text>
+        </TouchableHighlight>
       </View>
     );
   }
